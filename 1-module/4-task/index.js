5d81076f3f7e9000756f7cd7cd11a2cee975d57c
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-02-27 14:01:02 +0300

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,10 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+    let string = str.toUpperCase();
+    if ((string.includes("1XBET")) || (string.includes("XXX"))) {
+        return true;
+    } else {
+        return false;
+    }
 }
