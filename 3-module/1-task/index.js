55e3c7f2bbef79d20abc3aa2ca921ef7314d8d81
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-03-10 22:36:36 +0300

    Реализовала функцию, которая обрабатывает данные и выводит зарплаты пользователей

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,11 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    let str = ``;
+    for (let key of data) {
+      if (key['age'] <= age) {
+        str += `${key['name']}, ${key['balance']}\n`;
+      } 
+    }
+    return(str.slice(0,-1));
 }
