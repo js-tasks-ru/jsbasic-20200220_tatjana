20ac4b9225258ec21245531aa6ce22946cce4079
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-03-12 20:30:00 +0300

    Фильтрация по диапазону

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,19 @@
  * @param {number} b
  * @returns {number[]}
  */
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
+  let newArr = [];
+  for (let key of arr) {
+    if (key >= a && key <= b) {
+      newArr = newArr.concat(key);
+    }
+  }
+  return newArr;
 }
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr ); // [5,3,8,1] (без изменений)
