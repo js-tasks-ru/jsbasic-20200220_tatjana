24e63bed7a6a6bc67296e5c695828bbf7a1856a3
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-03-12 18:06:08 +0300

    функция преобразования строки

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,16 @@
  * @returns {string}
  */
 function camelize(str) {
+    let arr = str.split('-');
+    let removed = arr.splice(0,1);
+
+    arr = arr.map(function(el) {
+        let modified = el[0].toUpperCase()+el.slice(1);
+        return modified;
+    });
+
+    const resArr = removed.concat(arr);
+
+    const resString = resArr.join('');
+    return resString;
 }
