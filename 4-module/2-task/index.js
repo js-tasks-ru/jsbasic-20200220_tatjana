5a5e4981d070a88add3fcee6df6876a6d9956959
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-03-17 15:27:42 +0300

    Выделила ячейки по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,8 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    for (let i = 0; i < 5; i++) {
+        let cell = table.rows[i].cells[i];
+        cell.style.backgroundColor = 'red';
+    }
 }
