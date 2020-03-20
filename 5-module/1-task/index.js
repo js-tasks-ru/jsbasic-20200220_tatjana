066dd2062089b6216cb64eb209a919bd03c45a49
Tatiana Kostereva <tatjana.kostereva@yandex.ru>
2020-03-20 14:56:32 +0300

    Функция highlight

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,32 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+   for (let i = 0, row; row = table.rows[i]; i++) {
+      let cell = row.cells[3];
+      if (cell.hasAttribute('data-available') == false) {
+         row.setAttribute('hidden', true);
+      } else if (cell.getAttribute('data-available') == 'true') {
+         row.classList.add('available');
+      } else {
+         row.classList.add('unavailable');
+      }
+   }
+    
+   for (let i = 0, row; row = table.rows[i]; i++) {
+      let cell = row.cells[2];
+      if (cell.innerText == 'm') {
+         row.classList.add('male');
+      } else {
+         row.classList.add('female');
+      }
+   }
+    
+    
+   for (let i = 0, row; row = table.rows[i]; i++) {
+      let cell = row.cells[1];
+      if (+(cell.innerText) < 18) {
+         row.style.textDecoration = 'line-through';
+      } 
+   }
 }
